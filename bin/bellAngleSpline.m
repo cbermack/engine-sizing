c5560507b8f304866dab923bb3a06b9d95d3f8fb
cbermack <christopher.bermack@gmail.com>
2022-10-22 17:21:59 -0400

    Update bellAngleSpline.m

--- bin/bellAngleSpline.m
+++ bin/bellAngleSpline.m
@@ -8,19 +8,39 @@
 
 function [thetaNout, thetaEout] = bellAngleSpline(expRatioInput)
 %% theta outputs for expansion ratio input
-expRatio = [4 5 6 7 8 9 10 20 30];
-thetaN = [21.7 23.0 23.9 24.7 25.3 25.9 26.3 28.7 30.1];
-thetaE = [14.0 12.9 12.0 11.5 11.1 10.7 10.4 9.0 8.4];
+expRatio = [4 5 6 7 8 9 10 20 30 40 50];
+thetaN = [21.7 23.0 23.9 24.7 25.3 25.9 26.3 28.7 30.1 31.0 31.7];
+thetaE = [14.0 12.8 12.0 11.5 11.1 10.7 10.4 9.0 8.4 7.9 7.6];
 
-expRatio2 = 4:0.01:30; % finer expansion ratio for spline interpolation
+expRatio2 = 4:0.01:50; % finer expansion ratio for spline interpolation
 
 % spline of theta values
 thetaNspline = spline(expRatio,thetaN,expRatio2);
 thetaEspline = spline(expRatio,thetaE,expRatio2);
 
+%% extrapolation to lower expansion ratios
+
+% slopes for thetaN and thetaE from splines
+thetaNslope = (thetaNspline(2)-thetaNspline(1))/(expRatio2(2)-expRatio2(1));
+thetaEslope = (thetaEspline(2)-thetaEspline(1))/(expRatio2(2)-expRatio2(1));
+
+expRatioMin = 3; % minimum extrapolated expansion ratio
+
+% values for thetaN and thetaE at minimum expansion ratio
+thetaNmin = thetaN(1) + thetaNslope*(expRatioMin-expRatio(1));
+thetaEmin = thetaE(1) + thetaEslope*(expRatioMin-expRatio(1));
+
+% updating arrays and redoing spline with extrapolation
+expRatio = [expRatioMin, expRatio];
+thetaN = [thetaNmin, thetaN];
+thetaE = [thetaEmin, thetaE];
+expRatio2 = expRatioMin:0.01:50;
+thetaNspline = spline(expRatio,thetaN,expRatio2);
+thetaEspline = spline(expRatio,thetaE,expRatio2);
+
 % for loop to check input against expansion ratios
 m = 1;
-for n = 4:0.01:30
+for n = expRatioMin:0.01:50
     if expRatioInput == n
         break
     end
@@ -43,6 +63,8 @@ if plotInput == 'Y'
     semilogx(expRatio2,thetaEspline,'b-');
     semilogx(expRatio,thetaN,'ro');
     semilogx(expRatio,thetaE,'bo');
+    semilogx(expRatioInput,thetaNout,'r*');
+    semilogx(expRatioInput,thetaEout,'b*');
     hold off;
     title('Nozzle Contour Angles');
     xlabel('Expansion Ratio');
